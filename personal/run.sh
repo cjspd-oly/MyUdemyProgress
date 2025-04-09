a5201adebf802f05050cf314686f0d6bfe99f5cc
cjspd-oly <CJSPD.OLY+github@GMAIL.COM>
2025-04-09 13:18:52 +0530

    Update run.sh

--- personal/run.sh
+++ personal/run.sh
@@ -4,7 +4,7 @@
 cd ~/Documents/GitHub/MyUdemyProgress/personal/
 
 # Activate the virtual environment
-source .venv/bin/activate
+source venv/bin/activate
 
 # Run the Streamlit app
 streamlit run main.py --server.headless=true --server.port 8598
